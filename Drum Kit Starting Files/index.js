999ed2a78c415da9f6435fec3cbf7853aa80103e
Kanika <kanikasingh7482@gmail.com>
2023-08-23 23:32:48 +0530

    day-13

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -4,7 +4,20 @@ var noOfDrums = document.querySelectorAll(".drum").length;
 for(var i=0 ; i<noOfDrums ; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click" , function () {
      var buttonInnerHTML = this.innerHTML;
-        switch(buttonInnerHTML){
+     makeSound(buttonInnerHTML);
+     buttonAnimation(buttonInnerHTML);
+
+    });
+}
+document.addEventListener("keypress" , function(event){
+   makeSound(event.key);
+   buttonAnimation(event.key);
+
+
+} );
+function makeSound(key){
+
+          switch(key){
             case "d":
                 var tom1 = new Audio("sounds/tom-1.mp3");
     tom1.play();
@@ -38,6 +51,11 @@ case "w":
 
         }
    
-} );
-
+} 
+function buttonAnimation(currentKey){
+   var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
